380fb42629e383870048dbd8f5547240c42999a2
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-09 16:29:52 +0300

    Banner Query and Command handlers coded Some refactor stuff done

---
+++ Application/CarBook.Application/Features/CQRS/Handlers/CommandHandlers/BannerHandlers/UpdateBannerCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Application.Features.CQRS.Commands.BannerCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.CQRS.Handlers.CommandHandlers.BannerHandlers
+{
+    public class UpdateBannerCommandHandler
+    {
+        private readonly IRepository<Banner> repository;
+        public UpdateBannerCommandHandler(IRepository<Banner> repository)
+        {
+            this.repository = repository;
+        }
+        public async Task Handle(UpdateBannerCommand command)
+        {
+            var data = await repository.GetByIdAsync(command.BannerId);
+            data.Description = command.Description;
+            data.Title = command.Title;
+            data.VideoDescription = command.VideoDescription;
+            data.VideoUrl= command.VideoUrl;
+            data.CreatedDate= command.CreatedDate;
+            data.UpdatedDate= command.UpdatedDate;
+        }
+    }
+}
