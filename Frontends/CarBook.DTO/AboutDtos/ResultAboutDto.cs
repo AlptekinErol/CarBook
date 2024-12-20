a2a938684d4d83a77d0e6dc80c7d6e854211dc6e
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-20 21:24:23 +0300

    sa

--- Frontends/CarBook.DTO/AboutDtos/ResultAboutDto.cs
+++ Frontends/CarBook.DTO/AboutDtos/ResultAboutDto.cs
@@ -6,6 +6,7 @@
         public string Title { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
+        public string Test { get; set; }
 
     }
 }
