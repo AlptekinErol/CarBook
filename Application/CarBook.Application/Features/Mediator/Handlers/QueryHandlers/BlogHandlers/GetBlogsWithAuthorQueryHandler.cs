fd62d43e31c6b977983cb6ac60d7c2556bd3aa32
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-21 12:44:19 +0300

    Necessary QueryHandler, Result and Query classes created

---
+++ Application/CarBook.Application/Features/Mediator/Handlers/QueryHandlers/BlogHandlers/GetBlogsWithAuthorQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Queries.BlogQueries;
+using CarBook.Application.Features.Mediator.Results.BlogResults;
+using CarBook.Application.Interfaces.BlogInterfaces;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.QueryHandlers.BlogHandlers
+{
+    public class GetBlogsWithAuthorQueryHandler : IRequestHandler<GetBlogsWithAuthorQuery, List<GetBlogsWithAuthorQueryResult>>
+    {
+        private readonly IBlogRepository blogRepository;
+        public GetBlogsWithAuthorQueryHandler(IBlogRepository blogRepository)
+        {
+            this.blogRepository = blogRepository;
+        }
+
+        public async Task<List<GetBlogsWithAuthorQueryResult>> Handle(GetBlogsWithAuthorQuery request, CancellationToken cancellationToken)
+        {
+            var data = await blogRepository.GetBlogsWithAuthor();
+            return data.Select(x => new GetBlogsWithAuthorQueryResult
+            {
+                BlogId = x.Id,
+                AuthorId = x.AuthorId,
+                CategoryId = x.CategoryId,
+                CoverImageUrl = x.CoverImageUrl,
+                AuthorName = x.Author.AuthorName,
+                Title = x.Title,
+                CreatedDate = x.CreatedDate,
+                UpdatedDate = x.UpdatedDate,
+            }).ToList();
+        }
+    }
+}
---
+++ Application/CarBook.Application/Features/Mediator/Results/BlogResults/GetBlogsWithAuthorQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.Mediator.Results.BlogResults
+{
+    public class GetBlogsWithAuthorQueryResult
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string CategoryName { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
