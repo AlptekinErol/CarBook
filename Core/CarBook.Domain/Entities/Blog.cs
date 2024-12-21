6cf9d63b85c01e97a643675e9fa6acaf45ddf9dd
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-21 12:53:19 +0300

    migration added to Blog entity for Description property

--- Core/CarBook.Domain/Entities/Blog.cs
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -10,5 +10,6 @@ namespace CarBook.Domain.Entities
         public Author Author { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public string Description { get; set; }
     }
 }
