9351060ae1134c86a5b54e5c0cecda5703d3f2e5
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-22 18:54:23 +0300

    TagCloud endpoints implemented for crud

---
+++ Presentation/CarBook.Api/Controllers/TagCloudsController.cs
@@ -0,0 +1,53 @@
+﻿using CarBook.Application.Features.Mediator.Commands.TagCloudsCommands;
+using CarBook.Application.Features.Mediator.Queries.TagCloudsQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagCloudsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public TagCloudsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TagCloudList()
+        {
+            var data = await mediator.Send(new GetTagCloudQuery());
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTagCloud(int id)
+        {
+            var data = await mediator.Send(new GetTagCloudByIdQuery(id));
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTagCloud(CreateTagCloudCommand command)
+        {
+            await mediator.Send(command);
+            return Ok("TagCloud Created");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTagCloud(UpdateTagCloudCommand command)
+        {
+            await mediator.Send(command);
+            return Ok("TagCloud Updated");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveTagCloud(RemoveTagCloudCommand command)
+        {
+            await mediator.Send(command);
+            return Ok("TagCloud Deleted");
+        }
+    }
+}
