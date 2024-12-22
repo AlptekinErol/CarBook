544aa85587a3b1942678f7167e3ebf6abbde7567
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-22 20:20:56 +0300

    SingleBlogDetail ResultByBlogId implemented, each blog detail is listed with its id.

---
+++ Frontends/CarBook.DTO/BlogDtos/ResultBlogByIdDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CarBook.DTO.BlogDtos
+{
+    public class ResultBlogByIdDto
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
