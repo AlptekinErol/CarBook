00ccf877b3afbc6c3ac8262507e19f01dfcb82c1
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-22 20:21:15 +0300

    GetBlogById Handler and Result refactored for Description property

--- Application/CarBook.Application/Features/Mediator/Results/BlogResults/GetBlogByIdQueryResult.cs
+++ Application/CarBook.Application/Features/Mediator/Results/BlogResults/GetBlogByIdQueryResult.cs
@@ -7,6 +7,7 @@ namespace CarBook.Application.Features.Mediator.Results.BlogResults
         public int BlogId { get; set; }
         public string Title { get; set; }
         public string CoverImageUrl { get; set; }
+        public string Description { get; set; }
         public int AuthorId { get; set; }
         public int CategoryId { get; set; }
         public DateTime CreatedDate { get; set; }
