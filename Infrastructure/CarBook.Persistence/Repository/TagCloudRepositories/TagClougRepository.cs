6e19cf20188242e3e16801190a288c4df306f865
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-22 23:19:01 +0300

    TagCloud Handler Query and Result created and TagCloud Repository is created with its interface

---
+++ Infrastructure/CarBook.Persistence/Repository/TagCloudRepositories/TagClougRepository.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Application.Interfaces.TagCloudInterfaces;
+using CarBook.Domain.Entities;
+using CarBook.Persistence.Context;
+
+namespace CarBook.Persistence.Repository.TagCloudRepositories
+{
+    public class TagClougRepository : ITagCloudRepository
+    {
+        private readonly CarBookContext context;
+        public TagClougRepository(CarBookContext context)
+        {
+            this.context = context;
+        }
+        public  List<TagCloud> GetTagCloudsByBlogId(int id)
+        {
+            var data = context.TagClouds.Where(x => x.BlogId == id).ToList(); // TagCloudId eşit mi parametre id'ye => Listele
+            return data;
+        }
+    }
+}
