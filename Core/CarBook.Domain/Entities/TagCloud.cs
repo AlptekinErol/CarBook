3fea287c6b57aae0976db90b168070a27d049bd0
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-22 18:23:37 +0300

    Tag Clouds Entity implemented

---
+++ Core/CarBook.Domain/Entities/TagCloud.cs
@@ -0,0 +1,11 @@
+﻿using CarBook.Domain.Common;
+
+namespace CarBook.Domain.Entities
+{
+    public class TagCloud : EntityBase
+    {
+        public string Title { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ Infrastructure/CarBook.Persistence/Migrations/20241222144309_mig_tag_cloud_updated.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_tag_cloud_updated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TagCloudId",
+                table: "TagClouds",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TagClouds",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "TagClouds",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "TagClouds");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "TagClouds");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "TagClouds",
+                newName: "TagCloudId");
+        }
+    }
+}
