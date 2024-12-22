469982d011c27eb034df1ff7821810125a950f56
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-22 23:18:25 +0300

    TagCloud entity listed on frontend with blogId

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_SingleBlogDetailsMainTagCloudByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_SingleBlogDetailsMainTagCloudByBlogComponentPartial.cs
@@ -1,25 +1,26 @@
 ﻿using CarBook.DTO.BlogDtos;
+using CarBook.DTO.TagCloudDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
-    public class _SingleBlogDetailsMainComponentPartial : ViewComponent
+    public class _SingleBlogDetailsMainTagCloudByBlogComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory httpClientFactory;
-        public _SingleBlogDetailsMainComponentPartial(IHttpClientFactory httpClientFactory)
+        public _SingleBlogDetailsMainTagCloudByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
             this.httpClientFactory = httpClientFactory;
         }
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
             var client = httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7003/api/Blogs/" + id);
+            var responseMessage = await client.GetAsync($"https://localhost:7003/api/TagClouds/blog/" + id);
 
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var blogData = JsonConvert.DeserializeObject<ResultBlogByIdDto>(jsonData);
+                var blogData = JsonConvert.DeserializeObject<List<ResultGetTagCloudByBlogIdDto>>(jsonData);
 
                 return View(blogData);
             }
--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_SingleBlogDetailsTagCloudComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_SingleBlogDetailsTagCloudComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.DTO.BlogDtos;
+using CarBook.DTO.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _SingleBlogDetailsTagCloudComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory httpClientFactory;
+        public _SingleBlogDetailsTagCloudComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7003/api/TagClouds/blog/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var blogData = JsonConvert.DeserializeObject<List<ResultGetTagCloudByBlogIdDto>>(jsonData);
+
+                return View(blogData);
+            }
             return View();
         }
     }
