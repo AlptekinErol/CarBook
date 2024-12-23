15c23d997de63f25661b5c38ab26bbd6c0bd6866
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-23 22:43:20 +0300

    Comment Entity Added with Migration

--- Core/CarBook.Domain/Entities/Blog.cs
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -7,10 +7,12 @@ namespace CarBook.Domain.Entities
         public string Title { get; set; }
         public string CoverImageUrl { get; set; }
         public int AuthorId { get; set; }
-        public Author Author { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
         public string Description { get; set; }
         public List<TagCloud> TagClouds { get; set; }
+        public List<Comment> Comments { get; set; }
+        public Author Author { get; set; }
+        public Category Category { get; set; }
+
     }
 }
