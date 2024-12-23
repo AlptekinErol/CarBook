727ac7a495b6f7d845b3752e8e2e3b728d5b7ca9
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-23 22:07:36 +0300

    And its Dtos created

---
+++ Frontends/CarBook.DTO/AuthorDtos/ResultBlogByAuthorIdDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CarBook.DTO.AuthorDtos
+{
+    public class ResultBlogByAuthorIdDto
+    {
+        public int BlogId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorDescription { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
--- Frontends/CarBook.DTO/BlogDtos/ResultBlogsWithAuthorDto.cs
+++ Frontends/CarBook.DTO/BlogDtos/ResultBlogsWithAuthorDto.cs
@@ -9,6 +9,8 @@
         public string CoverImageUrl { get; set; }
         public string Description { get; set; }
         public int AuthorId { get; set; }
+        public string AuthorDescription { get; set; }
+        public string AuthorImageUrl { get; set; }
         public int CategoryId { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
