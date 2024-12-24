8f152a2d708e3d2292e7ca29e923f048b712a598
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-24 15:58:03 +0300

    CreateCar endpoin is implemented and tested

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -3,6 +3,7 @@ using CarBook.DTO.CarDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CarBook.WebUI.Controllers
@@ -45,6 +46,21 @@ namespace CarBook.WebUI.Controllers
                                                     }).ToList();
                 ViewBag.BrandValues = brandValues;
                 return View();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+        {
+            var client = httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCarDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7003/api/Cars", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
