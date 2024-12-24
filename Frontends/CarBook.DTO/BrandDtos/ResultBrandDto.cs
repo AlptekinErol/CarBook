de5e0213e01cc1c6eb4024ecdd26e22e365d82e8
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-24 15:14:06 +0300

    CreateCar method and its view pages is created with necessary files (dto)

---
+++ Frontends/CarBook.DTO/BrandDtos/ResultBrandDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarBook.DTO.BrandDtos
+{
+    public class ResultBrandDto
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,52 @@
+﻿using CarBook.DTO.BrandDtos;
+using CarBook.DTO.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7003/api/Cars/GetCarWithBrand");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+                return View(data);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7003/api/Brands");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+                List<SelectListItem> brandValues = (from x in data
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x.Name,
+                                                        Value = x.BrandId.ToString()
+                                                    }).ToList();
+                ViewBag.BrandValues = brandValues;
+                return View();
+            }
+            return View();
+        }
+    }
+}
