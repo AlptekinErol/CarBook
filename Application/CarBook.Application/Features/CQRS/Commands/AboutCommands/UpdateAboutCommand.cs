d8e6ea8b2f25e76c539b5db52cd12b2d4defea26
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-25 15:34:48 +0300

    Revert "Revert "unused packages removed from Commands""
    This reverts commit 5d4b2f81606d6ce2677f7294a8776f3bc9aeed33.

--- Application/CarBook.Application/Features/CQRS/Commands/AboutCommands/UpdateAboutCommand.cs
+++ Application/CarBook.Application/Features/CQRS/Commands/AboutCommands/UpdateAboutCommand.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarBook.Application.Features.CQRS.Commands.AboutCommands
+﻿namespace CarBook.Application.Features.CQRS.Commands.AboutCommands
 {
     public class UpdateAboutCommand
     {
-        public int Id { get; set; }
+        public int AboutId { get; set; }
         public string Title { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
