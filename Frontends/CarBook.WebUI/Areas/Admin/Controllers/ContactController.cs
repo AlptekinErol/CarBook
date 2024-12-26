49ce93cdc8a17ea28bf363fbae8b53694d275a6c
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-26 15:39:18 +0300

    Contact Controller added to Fe for Admin

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.DTO.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Contact")]
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        /// <summary>
+        /// Tüm Contact'ları listeleyen sayfayı getirir.
+        /// </summary>
+        /// <returns>Contact listesini içeren View döner.</returns>
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7003/api/Contacts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(data);
+            }
+            return View();
+        }
+    }
+}
