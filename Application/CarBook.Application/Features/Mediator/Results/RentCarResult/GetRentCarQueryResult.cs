a2ef181ebacf950040f2ae676a6ed4244f45de4d
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-29 19:19:29 +0300

    GetRentCar Handler and Result refactored

--- Application/CarBook.Application/Features/Mediator/Results/RentCarResult/GetRentCarQueryResult.cs
+++ Application/CarBook.Application/Features/Mediator/Results/RentCarResult/GetRentCarQueryResult.cs
@@ -3,5 +3,10 @@
     public class GetRentCarQueryResult
     {
         public int CarId { get; set; }
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public string BrandName { get; set; }
+        public decimal PricingAmount { get; set; }
     }
 }
+
