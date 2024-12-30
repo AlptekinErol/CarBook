b0410753f91b52d13fac579d90afd039f05b3641
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-30 17:18:03 +0300

    4 Statistic information is listed on ui page using be endpoints

---
+++ Frontends/CarBook.DTO/StatisticDtos/ResultStatisticDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBook.DTO.StatisticDtos
+{
+    public class ResultStatisticDto
+    {
+        public int CarCount { get; set; }
+        public int LocationCount { get; set; }
+        public int BrandCount { get; set; }
+        public int CarCountElectric { get; set; }
+    }
+}
