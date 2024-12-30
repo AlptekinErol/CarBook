795e156ef3e67236583dab21779bcf8c5037d739
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-30 16:39:07 +0300

    GetCarPricing Query, Handler and Result created

---
+++ Application/CarBook.Application/Features/Mediator/Handlers/QueryHandlers/CarPicingHandler/GetCarPricingWithTimePeriodQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Application.Features.Mediator.Queries.CarPricingQueries;
+using CarBook.Application.Features.Mediator.Results.CarPricingResults;
+using CarBook.Application.Interfaces.CarPricingInterfaces;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.QueryHandlers.CarPicingHandler
+{
+    public class GetCarPricingWithTimePeriodQueryHandler : IRequestHandler<GetCarPricingWithTimePeriodQuery, List<GetCarPricingWithTimerPeriodQueryResult>>
+    {
+        private readonly ICarPricingRepository carPricingRepository;
+        public GetCarPricingWithTimePeriodQueryHandler(ICarPricingRepository carPricingRepository)
+        {
+            this.carPricingRepository = carPricingRepository;
+        }
+        public async Task<List<GetCarPricingWithTimerPeriodQueryResult>> Handle(GetCarPricingWithTimePeriodQuery request, CancellationToken cancellationToken)
+        {
+            var data = await carPricingRepository.GetCarPricingWithTimePeriod();
+
+            return data.Select(x => new GetCarPricingWithTimerPeriodQueryResult
+            {
+                Model = x.Model,
+                CoverImageUrl = x.CoverImageUrl,
+                Brand = x.Brand,
+                DailyAmount = x.DailyAmount,
+                WeeklyAmount = x.WeeklyAmount,
+                MonthlyAmount = x.MonthlyAmount,
+            }).ToList();
+        }
+    }
+}
---
+++ Application/CarBook.Application/Features/Mediator/Results/CarPricingResults/GetCarPricingWithTimerPeriodQueryResult.cs
@@ -0,0 +1,12 @@
+﻿namespace CarBook.Application.Features.Mediator.Results.CarPricingResults
+{
+    public class GetCarPricingWithTimerPeriodQueryResult
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal DailyAmount { get; set; }
+        public decimal WeeklyAmount { get; set; }
+        public decimal MonthlyAmount { get; set; }
+    }
+}
