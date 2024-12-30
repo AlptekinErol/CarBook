5b874de772424f9a7c47dff30af33221a0f77f73
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-30 20:48:23 +0300

    CommentCount is listed on SingleBlogDetails page

--- Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.DTO.CommentDtos;
+using CarBook.DTO.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory httpClientFactory;
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7003/api/Comments/CommentByBlog?id=" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var blogData = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+
+                return View(blogData);
+            }
             return View();
         }
     }
