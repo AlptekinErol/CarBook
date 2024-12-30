3b161456564c66f256a35dbd80cdf08cdc7729e1
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-30 20:47:35 +0300

    CommentCount added to dto

--- Frontends/CarBook.DTO/BlogDtos/ResultBlogByIdDto.cs
+++ Frontends/CarBook.DTO/BlogDtos/ResultBlogByIdDto.cs
@@ -6,6 +6,7 @@
         public string Title { get; set; }
         public string CoverImageUrl { get; set; }
         public string Description { get; set; }
+        public int CommentCount { get; set; }
         public int AuthorId { get; set; }
         public int CategoryId { get; set; }
         public DateTime CreatedDate { get; set; }
