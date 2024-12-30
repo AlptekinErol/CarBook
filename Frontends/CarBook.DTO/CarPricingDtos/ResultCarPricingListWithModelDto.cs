95c52ff0b982f2ffcb0f97247879193e2f4b7332
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-30 16:39:16 +0300

    FrontEnd side is coded

---
+++ Frontends/CarBook.DTO/CarPricingDtos/ResultCarPricingListWithModelDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CarBook.DTO.CarPricingDtos
+{
+    public class ResultCarPricingListWithModelDto
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal HourlyAmount { get; set; }
+        public decimal DailyAmount { get; set; }
+        public decimal WeeklyAmount { get; set; }
+        public decimal MonthlyAmount { get; set; }
+    }
+}
---
+++ Frontends/CarBook.WebUI/Controllers/CarPricingController.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.DTO.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class CarPricingController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        public CarPricingController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Paketler";
+            ViewBag.v2 = "Araç Fiyat Paketleri";
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7003/api/CarPricings/CarPricingsWithTimePeriod");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+
+                return View(data);
+            }
+            return View();
+        }
+    }
+}
