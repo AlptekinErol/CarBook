efc487a6546d2fb3e38f995fe0e854e79ffd8022
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-12-30 20:47:25 +0300

    GetBlogById Handler and Result is refactored with GetCommentCount method

--- Application/CarBook.Application/Interfaces/CommentInterfaces/ICommentRepository.cs
+++ Application/CarBook.Application/Interfaces/CommentInterfaces/ICommentRepository.cs
@@ -5,5 +5,6 @@ namespace CarBook.Application.Interfaces.CommentInterfaces
     public interface ICommentRepository
     {
         Task<List<Comment>> GetCommentByBlogId(int id);
+        Task<int> GetCommentCount(int id);
     }
 }
